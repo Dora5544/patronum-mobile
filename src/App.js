a1c0ea5f79976b5e6acb5974bf7376cf1d3888f4
happyyuwei <happyyyuwei1994@qq.com>
2020-11-22 18:47:40 +0800

    add boot page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import { Logon } from "./Logon.js"
 import { LocationList } from "./List.js"
 // 使用路由跳转页面
 import { HashRouter as Router, Route, Link } from 'react-router-dom'
+import { BootPage } from "./boot";
 
 function App() {
+
   return (
     <Router>
+      <Route path="/" exact component={BootPage} />
       {/* 初始页面显示在list列表页面 */}
-      <Route path="/list" exact component={LocationList} />
+      {/* <Route path="/list" exact component={LocationList} /> */}
       {/* 跳转到登录页面 */}
       <Route path="/logon" exact component={Logon} />
     </Router>
--- src/boot.js
+++ src/boot.js
@@ -1,2 +1,18 @@
 import React from "react"
+import { LocationList } from "./List"
 
+class BootPage extends React.Component {
+
+
+    componentDidMount = () => {
+        // 使用get从后台获取数据
+    }
+
+    render = () => {
+        return <>
+            <LocationList></LocationList>
+        </>
+    }
+}
+
+export { BootPage }
