99d42e20b1ac7f2706c282358bca78a9b8378136
happyyuwei <happyyyuwei1994@qq.com>
2020-11-28 19:07:20 +0800

    fix the bug of error alert

--- src/List.js
+++ src/List.js
@@ -12,31 +12,33 @@ class LocationList extends React.Component {
 
     componentDidMount = () => {
         // 判断本地是否存有用户名和密码，如果没有，就跳转到登录页面进行登录
-        if (localStorage.getItem("username") === null && localStorage.getItem("userpassword") === null && localStorage.getItem("token") === null) {
+        if (localStorage.getItem("username") === null || localStorage.getItem("userpassword") === null || localStorage.getItem("token") === null) {
             window.location.href = "/patronum/#/logon"
-        }
-        // 使用get从后台获取数据
-        axios.get("https://happyyuwei.xyz:17615/rest/observer/location/all", {
-            // 将token放在header里面
-            headers: {
-                "token": localStorage.getItem("token")
-            }
-        })
-            // 请求成功，获取到数据
-            .then((response) => {
-                // data是一个数组里面嵌套对象的形式
-                let data = response.data;
-                console.log(data)
-                // 当从后台获取到数据，将数据放到状态location李敏
-                this.setState({
-                    loading: false,
-                    locations: data,
+        } else {
+            // 使用get从后台获取数据
+            axios.get("https://happyyuwei.xyz:17615/rest/observer/location/all", {
+                // 将token放在header里面
+                headers: {
+                    "token": localStorage.getItem("token")
+                }
+            })
+                // 请求成功，获取到数据
+                .then((response) => {
+                    // data是一个数组里面嵌套对象的形式
+                    let data = response.data;
+                    console.log(data)
+                    // 当从后台获取到数据，将数据放到状态location李敏
+                    this.setState({
+                        loading: false,
+                        locations: data,
+                    })
                 })
-            })
-            // 请求失败，打印出error
-            .catch((error) => {
-                console.log(error)
-            })
+                // 请求失败，打印出error
+                .catch((error) => {
+                    console.log(error)
+                    alert("连接服务器错误")
+                })
+        }
     }
 
     logout = () => {
@@ -83,7 +85,7 @@ class LocationList extends React.Component {
         let detailBody = <div className="list-loading">
             加载中...
             </div>
-            
+
         if (this.state.loading === false) {
             let itemList = [];
             for (let i = 0; i < this.state.locations.length; i++) {
